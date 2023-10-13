e2d6da4e77f485bc5ab34c7da48d962a44641d4c
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2023-10-13 22:56:47 +0200

    updating auth model

--- authentication/models.py
+++ authentication/models.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
 class User(AbstractUser):
-    zip_code = models.CharField(max_length=5, default='00000')
+    zip_code = models.CharField(max_length=5, default="00000")
+    profile_photo = models.ImageField(verbose_name="photo de profil")
+    follows = models.ManyToManyField("self", symmetrical=True, verbose_name="suit")
