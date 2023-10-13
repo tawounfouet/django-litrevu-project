dfdfacd6308af222ebb92bd631a15e9e2c66b8e6
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2023-10-13 23:23:14 +0200

    adding model to our review app

--- review/admin.py
+++ review/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Ticket, Review, UserFollows
+
+admin.site.register(Ticket)
+admin.site.register(Review)
+admin.site.register(UserFollows)
+
